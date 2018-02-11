efa5ea14938b05d89765180f6ccfae4d15efd41c
Robert Cauvy <robert.cauvy@wework.com>
2018-02-11 22:33:31 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,9 @@
 # Define display_board that accepts a board and prints
+
+board = [" "," "," "," "," "," "," "," "," "]
+
 # out the current state.
+
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]}"
   puts "----------"
